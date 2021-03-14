50b57921584220e57a6d3315baa635a1ba615b49
Abdul Mohaimen <mohaimen707@gmail.com>
2021-03-14 23:03:31 +0600

    Fresh all design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {HeaderComponent} from './header/header.component';
-import {IntroComponent} from './intro/intro.component';
-import {ContentComponent} from './content/content.component';
-import {TestimonialComponent} from './testimonial/testimonial.component';
-import {GalleryComponent} from './gallery/gallery.component';
-import {ClientComponent} from './client/client.component';
-import {PricingComponent} from './pricing/pricing.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
-  {path: 'home', component: HeaderComponent},
+/*  {path: 'home', component: HeaderComponent},
   {path: 'about', component: IntroComponent},
   {path: 'services', component: ContentComponent},
   {path: 'testimonials', component: TestimonialComponent},
   {path: 'gallery', component: GalleryComponent},
   {path: 'client', component: ClientComponent},
-  {path: 'pricing', component: PricingComponent},
+  {path: 'pricing', component: PricingComponent},*/
 ];
 
 @NgModule({
@@ -25,12 +18,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const RoutingComponents = [
-  IntroComponent,
-  GalleryComponent,
-  ContentComponent,
-  TestimonialComponent,
-  ClientComponent,
-  PricingComponent,
-  HeaderComponent
-];
+export const RoutingComponents = [];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { FooterComponent } from './footer/footer.component';
-import { NavComponent } from './nav/nav.component';
 import { AppRoutingModule, RoutingComponents } from './app-routing.module';
-import { ConfigService } from './services/config.service';
 
 @NgModule({
   declarations: [
     RoutingComponents,
     AppComponent,
-    NavComponent,
-    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
   ],
-  providers: [ConfigService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
