fd46889e76f08c2988d9b3038c7a4391de4be720
Abdul Mohaimen <mohaimen707@gmail.com>
2021-03-15 01:14:32 +0600

    Setup Tempating

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {BlogpostFeaturedComponent} from './blogpost/blogpost-featured/blogpost-featured.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: '', component: BlogpostFeaturedComponent},
 ];
 
 @NgModule({
