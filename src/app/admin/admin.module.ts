a200ea9e1aefafed34c10555a162405b51e854da
Abdul Mohaimen <mohaimen707@gmail.com>
2021-03-23 22:42:45 +0600

    auth functionality added

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
+import { AdminComponent } from './admin/admin.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { AdminBlogsComponent } from './admin-blogs/admin-blogs.component';
+import { AdminCategoriesComponent } from './admin-categories/admin-categories.component';
+import { AdminPagesComponent } from './admin-pages/admin-pages.component';
+import { ManageBlogsComponent } from './manage-blogs/manage-blogs.component';
+import { ManageCategoriesComponent } from './manage-categories/manage-categories.component';
+import { ManagePagesComponent } from './manage-pages/manage-pages.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [AdminComponent, AdminDashboardComponent, AdminBlogsComponent, AdminCategoriesComponent, AdminPagesComponent, ManageBlogsComponent, ManageCategoriesComponent, ManagePagesComponent],
   imports: [
     CommonModule,
     AdminRoutingModule
---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@angular/core';
+import {SharedService} from '../shared/shared.service';
+import {HttpClient, HttpResponse} from '@angular/common/http';
+import {catchError, map} from 'rxjs/operators';
+import {throwError} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  baseUrl = this.sharedService.baseUrl;
+  errorData: {};
+
+  isLoggedIn = false;
+
+  constructor(
+    private sharedService: SharedService,
+    private http: HttpClient
+  ) { }
+
+  login = (email: string, password: string) => {
+    return this.http.post<any>(this.baseUrl + '/api/auth/login', {email, password}).pipe(
+        map(user => {
+          if (user && user.access_token) {
+            localStorage.setItem('currentUser', JSON.stringify(user));
+            this.isLoggedIn = true;
+          }
+          console.log(user);
+        }),
+        catchError(this.handleError)
+    );
+  }
+
+  isLogin = () => {
+    if (localStorage.getItem('currentUser')) {
+      return true;
+    }
+    return false;
+  }
+
+  logout = () => {
+    localStorage.removeItem('currentUser');
+    this.isLoggedIn = false;
+  }
+
+  getAuthorizationToken = () => {
+    const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    return currentUser.access_token;
+  }
+
+  private handleError = (error: HttpResponse<any>) => {
+    if (error.error instanceof ErrorEvent) {
+      console.error('An error occurred:', error.error.message);
+    } else {
+      console.error(`Backend return code ${error.status}`);
+    }
+    this.errorData = {
+      errorTitle: 'Oops! Request failed.',
+      errorDesc: 'Something went wrong. Please try again later.'
+    };
+    return throwError(this.errorData);
+  }
+}
