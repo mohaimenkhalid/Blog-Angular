879ca94be1b63f734259043e24ea93cbec8dc240
Abdul Mohaimen <mohaimen707@gmail.com>
2021-03-28 01:06:37 +0600

    login & registration page, auth interceptor adds

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {BlogpostFeaturedComponent} from './blogpost/blogpost-featured/blogpost-featured.component';
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
---
+++ src/app/http-interceptors/auth-interceptor.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
+import { AuthService } from '../auth/auth.service';
+import { Observable } from 'rxjs';
+
+@Injectable()
+
+export class AuthInterceptor {
+  constructor(private authService: AuthService) {
+  }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (this.authService.isLoggedIn) {
+      const authToken = this.authService.getAuthorizationToken();
+      req = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${authToken}`
+        }
+      });
+    }
+    return next.handle(req);
+  }
+}
