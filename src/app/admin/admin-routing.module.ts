b0fd3d225aed9251dc3b729df00072de8ce9b47e
Abdul Mohaimen <mohaimen707@gmail.com>
2021-04-08 00:56:44 +0600

    blog create form and routing adds

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -5,6 +5,7 @@ import { AuthGuard } from '../auth/auth.guard';
 import {AdminComponent} from './admin/admin.component';
 import {ManageBlogsComponent} from './manage-blogs/manage-blogs.component';
 import {AdminDashboardComponent} from './admin-dashboard/admin-dashboard.component';
+import {BlogFormComponent} from './blog-form/blog-form.component';
 
 const routes: Routes = [
   {
@@ -13,6 +14,8 @@ const routes: Routes = [
       { path: '',
         children: [
           { path: 'blogs', component: ManageBlogsComponent},
+          { path: 'blogs/create', component: BlogFormComponent},
+          { path: 'blogs/edit/:id', component: BlogFormComponent},
           { path: 'categories', component: ManageBlogsComponent},
           { path: '', component: AdminDashboardComponent},
         ]
